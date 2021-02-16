ba62143abcb006081101a4df8cb1044c30455dca
Ayrat <ayrat1@mail.ru>
2021-02-16 07:19:50 +0300

    add test method

--- src/main/java/de/springcloud/SpringCloudApplication.java
+++ src/main/java/de/springcloud/SpringCloudApplication.java
@@ -2,6 +2,12 @@ package de.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @SpringBootApplication
 public class SpringCloudApplication {
@@ -11,3 +17,37 @@ public class SpringCloudApplication {
 	}
 
 }
+
+@Entity
+class Cat {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+
+	public Cat() {
+	}
+
+	public Cat(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "Cat{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
+
+@RepositoryRestResource
+interface CatRepository extends JpaRepository<Cat, Long> {}
